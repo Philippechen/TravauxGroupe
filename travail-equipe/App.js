97d63f403d8963f0224ccbba8628ef2abc187468
Enlong Chen <linuxfile@gmail.com>
2023-05-31 22:03:12 -0400

    UI complete

--- travail-equipe/App.js
+++ travail-equipe/App.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, TextInput, Button, FlatList, Alert} from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, FlatList, Alert, Pressable} from 'react-native';
 import Constants from 'expo-constants';
 import Header from './Header';
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const [idx, setIdx] = useState(0);
-  const [idxSelected, setIdxSelected] = useState(0);
-  const [session, setSession] = useState("");
-  const [nouCours, setNouCours] = useState([]);
+  const [data, setData] = useState([]);              //Json
+  const [idx, setIdx] = useState(0);                 //index de donné trouvé par Id, Le utilisateur ne le choisisse pas forcément
+  const [idxSelected, setIdxSelected] = useState(0); //Index choisi
+  const [texte, setTexte] = useState("00");          //Pour initializer TexteInput d'Id
+  const [session, setSession] = useState("");        //Pour Afficher information de session
+  const [nouCours, setNouCours] = useState([]);      //Pour savoir le nom de nouvelle cours
   const [info, setInfo] = useState("Aucun étudiant sélectionné");
   const [msg, setMsg] = useState("Confirmez votre sélection");
 
@@ -23,8 +24,11 @@ export default function App() {
     getStudents();
   }, []);
 
+  //Trouver l'index de donné
+  //Il n'est pas en ordre nécessairement, donc on ne peut pas utiliser id comme index
   const idToName = (id) => {
     setMsg("Confirmez votre sélection");
+    setTexte(id);
     let idx = data.findIndex(it=>it.id_etudiant == id);
     if (idx > -1) {
       setIdx(idx);
@@ -62,23 +66,40 @@ export default function App() {
     }
     alert(msg);
   }
+  
   return (
     <View style={styles.container}>
       <Header titre = "INSCRIPTION AUX COURS" couleurFond = "blue"/>
+
       <View style={styles.select}>
-        <Text>Id:</Text>
-        <TextInput style={{borderWidth:1}} onChangeText={idToName} />
-        <Text>{info}</Text>
-        <Button title="SÉLECTIONNER UN ÉTUDIANT" onPress={()=>{setMsg("Élève sélectionné");setIdxSelected(idx);}}/>
-        <Text style={{color:'red'}}>{msg}</Text>
+        <Text style={styles.font}>Id:</Text>
+        <TextInput style={{borderWidth:1}} keyboardType="number-pad" onChangeText={idToName} value={texte} />
+        <Text style={styles.font}>{info}</Text>
+        <Pressable   style={styles.press}
+          onPress={()=>{setMsg("Élève sélectionné");setIdxSelected(idx);}}>
+          <Text style={styles.pressText}>SÉLECTIONNER UN ÉTUDIANT</Text>
+        </Pressable>
+        <Text style={[styles.font, {color:'red'}]}>{msg}</Text>
       </View>
-      <View style={{marginTop:20}}>
-        <Text>Session:</Text>
+
+      <View style={styles.cours}>
+        <Text style={styles.font} >Session:</Text>
         <TextInput style={{borderWidth:1, width:'98%'}} value={session} />
-        <Text>Enregistrer élève au cours:</Text>
+        <Text style={styles.font}>Enregistrer élève au cours:</Text>
         <TextInput style={{borderWidth:1, width:'98%'}} onChangeText={addCours} value={nouCours} />
-        <Button style={styles.button} title="ENREGISTRER" onPress={register} />
-        <Button style={styles.button} title="AFFICHER" onPress={afficher}/>
+      </View>
+
+      <View style={styles.buttonView}>
+        <Pressable   style={styles.press}
+          onPress={register}>
+          <Text  style={styles.pressText}>ENREGISTRER</Text>
+        </Pressable>
+      </View>
+      <View style={styles.buttonView}>
+        <Pressable   style={styles.press}
+          onPress={afficher}>
+          <Text style={styles.pressText}>AFFICHER</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -87,26 +108,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
   select:{
+    justifyContent:'space-around',
     backgroundColor:'lightgreen',
+    height:210,
     padding: 20,
     fontSize: 18
   },
-  button: {
-    margin:100,
-    padding:100,
+  cours:{
+    justifyContent:'space-around',
+    height:130,
+    fontSize: 18,
+    marginTop:10, 
+    marginBottom:40
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 20,
-    backgroundColor:'blue',
+  press: {
+    padding:8,
+    borderRadius:3,
+    backgroundColor:'dodgerblue',
+  },
+  pressText:{
     color:'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    textAlign:'center'
+  },
+  buttonView:{
+    marginTop:20
+  },
+  font:{
+    fontWeight: 'bold'
   },
 });
